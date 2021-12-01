f00a9578d5775cb22192e12ede6008ea649dd8fa
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-12-01 06:00:51 +0530

    Add Switch Statement

---
+++ 5-Control-Flow-Statements/Switch-Statement/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value = 3;
+        if(value == 1) {
+            System.out.println("Value was 1");
+        } else if(value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+
+        int switchValue = 4;
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3:case 4:case 5:
+                System.out.println("Was a 3 or 4 or 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+    }
+}
