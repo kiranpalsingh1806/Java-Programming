07a56f045c16e30d38bad8eb93f94902eb53fffa
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-12-21 11:11:18 +0530

    Add OOP Starter Code

---
+++ 5-Control-Flow-Statements/Reading-User-Input/src/com/company/InputCalculator.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+
+        while(scanner.hasNextInt()){
+            sum += scanner.nextInt();
+            count++;
+            scanner.nextLine();
+        }
+        System.out.println("SUM = " + sum + " AVG = " + Math.round((double) sum/count));
+        scanner.close();
+    }
+}
+
--- 6-Object-Oriented-Programming-Part-1/Classes/src/com/company/Main.java
+++ 6-Object-Oriented-Programming-Part-1/Classes/src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
 public class Main {
+
     public static void main(String[] args) {
-        InputCalculator.inputThenPrintSumAndAverage();
+	    Car porsche = new Car();
+        Car holder = new Car();
     }
 }
